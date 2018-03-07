4e0f16ecf69120cdeb35c96431b875c57fb94999
Stefanie C <stef.c85@gmail.com>
2018-03-07 15:30:24 +0800

    Work page

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -21,4 +21,68 @@ function theme_enqueue_styles() {
 	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
     wp_enqueue_script( 'fancybox-scripts', get_stylesheet_directory_uri() . '/js/jquery.fancybox.min.js', array(), false);
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+    wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/js/main.js', array(), $the_theme->get( 'Version' ), true );
 }
+
+// Register Case Study Custom Post Type
+function case_study_cpt() {
+
+    $labels = array(
+        'name'                  => _x( 'Case Studies', 'Post Type General Name', 'text_domain' ),
+        'singular_name'         => _x( 'Case Study', 'Post Type Singular Name', 'text_domain' ),
+        'menu_name'             => __( 'Case Studies', 'text_domain' ),
+        'name_admin_bar'        => __( 'Case Studies', 'text_domain' ),
+        'archives'              => __( 'Case Studies Archives', 'text_domain' ),
+        'attributes'            => __( 'Case Study Attributes', 'text_domain' ),
+        'parent_item_colon'     => __( 'Parent Case Study:', 'text_domain' ),
+        'all_items'             => __( 'All Case Studies', 'text_domain' ),
+        'add_new_item'          => __( 'Add New Case Study', 'text_domain' ),
+        'add_new'               => __( 'Add New', 'text_domain' ),
+        'new_item'              => __( 'New Case Study', 'text_domain' ),
+        'edit_item'             => __( 'Edit Case Study', 'text_domain' ),
+        'update_item'           => __( 'Update Case Study', 'text_domain' ),
+        'view_item'             => __( 'View Case Study', 'text_domain' ),
+        'view_items'            => __( 'View Case Studies', 'text_domain' ),
+        'search_items'          => __( 'Search Case Study', 'text_domain' ),
+        'not_found'             => __( 'Not found', 'text_domain' ),
+        'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+        'featured_image'        => __( 'Featured Image', 'text_domain' ),
+        'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+        'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+        'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+        'insert_into_item'      => __( 'Insert into Case Study', 'text_domain' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this Case Study', 'text_domain' ),
+        'items_list'            => __( 'Case Studies list', 'text_domain' ),
+        'items_list_navigation' => __( 'Case Studies list navigation', 'text_domain' ),
+        'filter_items_list'     => __( 'Filter Case Studies list', 'text_domain' ),
+    );
+    $rewrite = array(
+        'slug'                  => 'work',
+        'with_front'            => true,
+        'pages'                 => true,
+        'feeds'                 => true,
+    );
+    $args = array(
+        'label'                 => __( 'Case Study', 'text_domain' ),
+        'description'           => __( 'Custom Post Type for Case Study', 'text_domain' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'menu_icon'             => 'dashicons-images-alt',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'rewrite'               => $rewrite,
+        'capability_type'       => 'page',
+    );
+    register_post_type( 'case_study_cpt', $args );
+
+}
+add_action( 'init', 'case_study_cpt', 0 );
---
+++ wp-content/themes/understrap-child/page-work.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Template Name: Work
+ *
+ * @package understrap
+ */
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+
+global $post;
+$post_slug=$post->post_name;
+$allselect = $websiteselect = $creativeselect = $brandingselect = $digitalselect = '';
+
+switch ($post_slug) {
+	case 'work':
+	$allselect = 'selected';
+	break;
+	case 'website':
+	$websiteselect = 'selected';
+	break;
+	case 'creative':
+	$creativeselect = 'selected';
+	break;
+	case 'branding':
+	$brandingselect = 'selected';
+	break;
+	case 'digital':
+	$digitalselect = 'selected';
+	break;
+	
+	default:
+	break;
+}
+
+?>
+
+<div class="wrapper" id="page-wrapper">
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<section class="our-work-filter container" id="content" tabindex="-1">
+
+			<div class="row">
+
+				<main class="site-main" id="main">
+
+					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+
+					<div class="filter">
+						<span class="light-text">Filter Work By:</span>
+						<a href=".." class="filter-text <?=$allselect?>">All</a>
+						<a href="website" class="filter-text <?=$websiteselect?>">Website</a>
+						<a href="creative" class="filter-text <?=$creativeselect?>">Creative</a>
+						<a href="branding" class="filter-text <?=$brandingselect?>">Branding</a>
+						<a href="digital" class="filter-text <?=$digitalselect?>">Digital</a>
+					</div>
+
+				</main><!-- #main -->
+
+			</div><!-- .row -->
+
+		</section><!-- Container end -->
+
+		<section class="our-work our-work-grid container-fluid">
+			<?php
+			if($post_slug === 'work') {
+				$args = array(
+					'post_type' => 'case_study_cpt',
+					'post_status' => 'publish',
+					'posts_per_page' => -1,
+				);
+			} else {				
+				$args = array(
+					'post_type' => 'case_study_cpt',
+					'post_status' => 'publish',
+					'posts_per_page' => -1,
+					'meta_query' => array (
+						array (
+							'key' => 'case_study_type',
+							'value' => $post_slug,
+							'compare' => 'LIKE'
+						)
+					),
+				);
+			}
+			$the_query = new WP_Query($args);
+
+			if($the_query->have_posts()) :
+				$count = 0; 
+				while($the_query->have_posts()) : $the_query->the_post(); 
+					$image = get_field('cover_image');
+					$size = 'full';
+					$types = get_field('case_study_type');
+					foreach($types as $type) :
+						$typeclass .= $type['value'].' ';
+					endforeach;
+
+					if($count === 0) echo '<div class="row">';
+					?>
+					<!-- TODO: add link to case study -->
+					<a href="#" class="our-work-single <?=$typeclass?>col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12" style="background:url('<?php echo $image['url']; ?>');background-size:cover;height:350px">
+						<div class="our-work-single-about">
+							<h3><?php the_title(); ?></h3>
+							<p><?php echo get_field('case_study_summary'); ?></p>
+							<p class="light-text">
+								<?php
+								foreach($types as $type) :
+									echo '<span class="our-work-single-type '.$type['value'].'">'.$type['label'].'</span>';
+								endforeach;
+								?>
+							</p>
+						</div>
+					</a>
+					<?php 
+					$count++;
+
+					if($count % 2 == 0) { echo '</div>'; $count=0; }
+				endwhile;
+			endif;
+			?>
+		</section>
+
+		<section class="work-clients container-fluid" id="content" tabindex="-1">
+
+			<div class="row">
+
+				<div class="container">
+
+					<div class="row">
+
+						<h2>Our Clients</h2>
+
+					</div>
+
+					<div class="row work-clients-container">
+
+						<?php
+						if(have_rows('client', 49)) :
+							while(have_rows('client', 49)) : the_row();
+								echo '<img src="'.get_sub_field('client_logo').'" class="work-clients-logo" title="'.get_sub_field('client_name').'" alt="'.get_sub_field('client_name').'">';
+							endwhile;
+						endif;
+						?>
+
+					</div>
+					
+				</div>
+
+			</div>
+
+		</section>
+
+	</div>
+
+<?php endwhile; // end of the loop. ?>
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
